9251dcd192c7993aa69b9a7aca67359eae42796f
ricfrank <riccardo.franconi@soisy.it>
2022-06-14 10:18:44 +0200

    implememtimg pacman effect

--- src/domain.rs
+++ src/domain.rs
@@ -3,7 +3,55 @@
         pub w: usize,
     }
 
-    #[derive(PartialEq, Debug)]
+    impl Planet {
+        fn next_position(&self, rover: &Rover) -> Position {
+            match rover.direction {
+                Direction::North => {
+                    if self.h == rover.position.y + 1 {
+                        Position { y: 0, ..rover.position }
+                    } else {
+                        Position { y: rover.position.y + 1, ..rover.position }
+                    }
+                },
+                Direction::South => {
+                    if rover.position.y == 0 {
+                        Position { y: self.h - 1, ..rover.position }
+                    } else {
+                        Position { y: rover.position.y - 1, ..rover.position }
+                    }
+                }
+                Direction::East => {
+                    if self.w == rover.position.x + 1 {
+                        Position {x: 0, ..rover.position}
+                    } else {
+                        Position {x: rover.position.x + 1, ..rover.position}
+                    }
+                }
+                Direction::West => {
+                    if rover.position.x == 0 {
+                        Position {x: self.w - 1, ..rover.position}
+                    } else {
+                        Position {x: rover.position.x - 1, ..rover.position}
+                    }
+                }
+            }
+
+        }
+
+        fn prev_position(&self, rover: &Rover) -> Position {
+            let opposite_direction = match rover.direction {
+                Direction::North => Direction::South,
+                Direction::South => Direction::North,
+                Direction::West => Direction::East,
+                Direction::East => Direction::West,
+            };
+
+            let new_rover = Rover {position: rover.position, direction: opposite_direction};
+            self.next_position(&new_rover)
+        }
+    }
+
+    #[derive(PartialEq, Debug, Copy, Clone)]
     struct Position {
         pub x: usize,
         pub y: usize,
@@ -56,22 +104,12 @@
     }
 
     fn move_forward(planet: Planet, rover: Rover) -> Rover {
-        let position = match rover.direction {
-            Direction::North => Position { x: rover.position.x, y: rover.position.y + 1 },
-            Direction::East => Position { x: rover.position.x + 1, y: rover.position.y },
-            Direction::South => Position { x: rover.position.x, y: rover.position.y - 1 },
-            Direction::West => Position { x: rover.position.x - 1, y: rover.position.y },
-        };
+        let position = planet.next_position(&rover);
         Rover { position, ..rover}
     }
 
     fn move_backward(planet: Planet, rover: Rover) -> Rover {
-        let position = match rover.direction {
-            Direction::North => Position { x: rover.position.x, y: rover.position.y - 1 },
-            Direction::East => Position { x: rover.position.x - 1, y: rover.position.y },
-            Direction::South => Position { x: rover.position.x, y: rover.position.y + 1 },
-            Direction::West => Position { x: rover.position.x + 1, y: rover.position.y },
-        };
+        let position = planet.prev_position(&rover);
         Rover { position, ..rover}
     }
 
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod tests {
 
     #[test]
     fn rotate_right_command() {
-        let planet = Planet { h: 5, w: 4 };
+        let planet = Planet { w: 4, h: 5};
         let rover = Rover::new(0, 0, "N");
         let rover = execute(Command::TurnRight, planet, rover);
 
@@ -24,7 +24,7 @@ mod tests {
 
     #[test]
     fn rotate_left_command() {
-        let planet = Planet { h: 5, w: 4 };
+        let planet = Planet { w: 4, h: 5};
         let rover = Rover::new(0, 0, "N");
         let rover = execute(Command::TurnLeft, planet, rover);
 
@@ -33,7 +33,7 @@ mod tests {
 
     #[test]
     fn move_forward_command() {
-        let planet = Planet { h: 5, w: 4 };
+        let planet = Planet { w: 4, h: 5};
         let rover = Rover::new(0, 0, "N");
         let rover = execute(Command::MoveForward, planet, rover);
 
@@ -42,10 +42,82 @@ mod tests {
 
     #[test]
     fn move_backward_command() {
-        let planet = Planet { h: 5, w: 4 };
+        let planet = Planet { w: 4, h: 5};
         let rover = Rover::new(0, 1, "N");
         let rover = execute(Command::MoveBackward, planet, rover);
 
         assert_eq!(rover, Rover::new(0, 0, "N"));
+    }
+
+    #[test]
+    fn pacman_effect_north_forward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 4, "N");
+        let rover = execute(Command::MoveForward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 0, "N"));
+    }
+
+    #[test]
+    fn pacman_effect_sud_forward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 0, "S");
+        let rover = execute(Command::MoveForward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 4, "S"));
+    }
+
+    #[test]
+    fn pacman_effect_east_forward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(3, 0, "E");
+        let rover = execute(Command::MoveForward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 0, "E"));
+    }
+
+    #[test]
+    fn pacman_effect_west_forward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 0, "W");
+        let rover = execute(Command::MoveForward, planet, rover);
+
+        assert_eq!(rover, Rover::new(3, 0, "W"));
+    }
+
+    #[test]
+    fn pacman_effect_north_backward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 4, "S");
+        let rover = execute(Command::MoveBackward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 0, "S"));
+    }
+
+    #[test]
+    fn pacman_effect_south_backward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 0, "N");
+        let rover = execute(Command::MoveBackward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 4, "N"));
+    }
+
+    #[test]
+    fn pacman_effect_east_backward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(3, 0, "W");
+        let rover = execute(Command::MoveBackward, planet, rover);
+
+        assert_eq!(rover, Rover::new(0, 0, "W"));
+    }
+
+    #[test]
+    fn pacman_effect_west_backward() {
+        let planet = Planet { w: 4, h: 5};
+        let rover = Rover::new(0, 0, "E");
+        let rover = execute(Command::MoveBackward, planet, rover);
+
+        assert_eq!(rover, Rover::new(3, 0, "E"));
     }
 }
